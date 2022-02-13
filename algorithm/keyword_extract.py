bd15dc676057cb881434d69c57de62bced32285b
张天纬 <1837669410@qq.com>
2022-02-13 22:06:50 +0800

    updata code

--- algorithm/keyword_extract.py
+++ algorithm/keyword_extract.py
@@ -3,8 +3,44 @@ import numpy as np
 import jieba
 import matplotlib.pyplot as plt
 
-def tf_idf(text, Top_K=10, return_v2i=False, use_visual=False):
-    text = [" ".join(jieba.lcut(c)) for c in text]
+def tf_idf(text, Top_K=10, return_v2i=False, return_idf_=True, return_tfidf=False, use_visual=False):
+    '''
+    文本关键词提取
+    Args:
+        text: 待提取的文本
+        Top_K: 返回几个TF/IDF权重最大的关键词，默认值为10
+        return_v2i: 是否返回{v:i}字典，默认为False
+        return_idf_：是否返回idf
+        return_tfidf：是否返回tfidf
+        use_visual: 是否可视化TF-IDF，默认为False
+
+    Returns:
+        word：关键词
+        score：关键词的tf-idf值
+        vectorizer.vocabulary_：{word_name:tfidf}
+        idf_：idf
+
+    Examples:
+        // 1、return_idf_=True和use_visual=True时数据太大时不建议开启可视化
+        >>> import TianNLP as TN
+        >>> x_train = [
+            "it is a good day, I like to stay here",
+            "I am happy to be here",
+            "I am bob",
+            "it is sunny today",
+            "I have a party today",
+        ]
+        >>> name, score = TN.tf_idf(x_train, return_tfidf= True ,use_visual=True)
+        >>> print(name, score)
+        // 2、return_tfidf=True，只建议单个句子单个句子的输入，或者投入整个文章，不建议开启可视化
+        >>> import TianNLP as TN
+        >>> x_train = [
+            "it is a good day, I like to stay here",
+        ]
+        >>> name, score = TN.tf_idf(x_train, return_tfidf= True ,use_visual=True)
+        >>> print(name, score)
+    '''
+    text = [" ".join(jieba.cut(c)) for c in text]
 
     vectorizer = TfidfVectorizer()
     tfidf = vectorizer.fit_transform(text)
@@ -13,18 +49,20 @@ def tf_idf(text, Top_K=10, return_v2i=False, use_visual=False):
     feature_names = vectorizer.get_feature_names()
     name, score = [], []
     for i in range(tfidf.shape[0]):
-        tfidf_index = tfidf[i, :].nonzero()[1]
+        tfidf_index = tfidf[i,:].nonzero()[1]
         for j in tfidf_index:
             name.append(feature_names[j])
             score.append(tfidf[i,j])
     name, score = np.array(name)[np.argsort(score)][::-1], np.array(score)[np.argsort(score)][::-1]
 
-    show_tf_idf(tfidf.todense(), [v for v in vectorizer.vocabulary_.keys()])
-
+    if use_visual:
+        show_tf_idf(tfidf.todense(), [v for v in vectorizer.vocabulary_.keys()])
     if return_v2i:
-        return name[:Top_K], score[:Top_K], vectorizer.vocabulary_
-
-    return name[:Top_K], score[:Top_K]
+        return vectorizer.vocabulary_
+    if return_tfidf:
+        return [(n, s) for n, s in zip(name[:Top_K], score[:Top_K])]
+    if return_idf_:
+        return {n: v for n, v in zip(vectorizer.get_feature_names(), vectorizer.idf_)}
 
 def show_tf_idf(tfidf_matrix, vocab):
     plt.imshow(tfidf_matrix, cmap="YlGn")
@@ -34,22 +72,12 @@ def show_tf_idf(tfidf_matrix, vocab):
     plt.show()
 
 if __name__ == "__main__":
-    x_train = docs = [
+    x_train = [
         "it is a good day, I like to stay here",
         "I am happy to be here",
         "I am bob",
         "it is sunny today",
         "I have a party today",
-        "it is a dog and that is a cat",
-        "there are dog and cat on the tree",
-        "I study hard this morning",
-        "today is a good day",
-        "tomorrow will be a good day",
-        "I like coffee, I like book and I like apple",
-        "I do not like it",
-        "I am kitty, I like bob",
-        "I do not care who like bob, but I like kitty",
-        "It is coffee time, bring your cup",
     ]
-    name, score = tf_idf(x_train)
+    name, score = tf_idf(x_train, return_tfidf=True)
     print(name, score)
